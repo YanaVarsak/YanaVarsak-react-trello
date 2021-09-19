adccfc13f2dbf11b66af34b37313651a24a44933
YanaVarsak <85955470+YanaVarsak@users.noreply.github.com>
2021-09-19 23:24:14 +0300

    finish_hw_2

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <span>Hello World</span>
+      </header>
+    </div>
+  );
+}
+
